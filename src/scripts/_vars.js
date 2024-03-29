20f51bcc865e135b53f54a1698f63ca23da71bdd
andrewdreamer13 <isaevandrey9@gmail.com>
2024-03-29 07:27:49 +0200

    Update 2024-03-29T05:27:07.589Z

--- src/scripts/_vars.js
+++
@@ -1,1 +0,0 @@
-export const mainPage = document.querySelector('.index-html');
--- src/scripts/main.js
+++
@@ -1,8 +0,0 @@
-/* main.js */
-import './_preloader';
-import './_header';
-import './_inputmask';
-import './_modal';
-import './_sliders';
-import './_scrolling';
-
